c4eec01167c901a0f2bf5fd6983f4acf05916a0b
SithuSoe153 <sithu032001@gmail.com>
2023-01-10 12:57:15 +0630

    1

---
+++ (4)– Strings & Arrays.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Sithu Soe';
+$role = "Software Engineer";
+$company = "Acme Inc";
+
+$name1 = str_replace("Soe", "Smart", $name);
+
+echo "\"$name1\" is a $role and working at \$$company.";
+echo "C:\\xampp\\htdocs";
